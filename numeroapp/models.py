2d86ccf5ef04bed250a159cc6ab794ce02e7609a
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-13 13:49:10 +0300

    perf: Update models - Adds project class

--- numeroapp/models.py
+++ numeroapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class numero_Project(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField(max_length=200)
+    image=models.ImageField(upload_to='images/')
+    url=models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+    
