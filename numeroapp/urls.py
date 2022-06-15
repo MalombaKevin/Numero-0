224c265f9724182a21b8a66cdf96a4a8d6353f6b
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-15 08:53:32 +0300

    Perf: Media settings - Adds settings to help load the image

--- numeroapp/urls.py
+++ numeroapp/urls.py
@@ -1,10 +1,16 @@
-#importations
+# importations
+from django.conf import settings
 from django.urls import path
 from django import views
 from . import views
+from django.conf.urls.static import static
 
-#urls paths
+# urls paths
 urlpatterns = [
-path('', views.index, name='index'),
-path('newProject', views.add_project, name='new_project'),
+    path('', views.index, name='index'),
+    path('newProject', views.add_project, name='new_project'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
