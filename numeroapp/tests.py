27ad8e66b1d8576fde9edc39a248629f9375cb67
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-17 11:23:39 +0300

    perf: Test - Implements tests to check the operationability of the classes in project's models

--- numeroapp/tests.py
+++ numeroapp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
+from numeroapp.models import numero_Profile, numero_Project
+
 # Create your tests here.
+def numero_Project_test():
+    def setup(self):
+        self.new_numero_project = numero_Project(name='Test Project', description='Test Description', image='Test Image')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_numero_project, numero_Project))
+    
+    def test_save_numero_project(self):
+        self.new_numero_project.save_numero_project()
+        numero_projects = numero_Project.objects.all()
+        self.assertTrue(len(numero_projects) > 0)
+
+def numero_Profile_test():
+    def setup(self):
+        self.new_numero_profile = numero_Profile(name='Test Profile', description='Test Description', image='Test Image')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_numero_profile, numero_Profile))
+    
+    def test_save_numero_profile(self):
+        self.new_numero_profile.save_numero_profile()
+        numero_profiles = numero_Profile.objects.all()
+        self.assertTrue(len(numero_profiles) > 0)
